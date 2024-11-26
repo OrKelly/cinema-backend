001330588d70e77dd6c61c9f1aa10449e08ff0d9
mrlawsuit <opronin1992@gmail.com>
2024-11-26 11:08:04 +0200

    add Enums for films model

---
+++ core/enums/films.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class AgeRatingEnum(Enum):
+    NO_RESTRICTION = (1, 'Без ограничений, 0+')
+    KIDS = (2, '6+')
+    TEEN = (3, '12+')
+    YOUNG = (4, '16+')
+    ADULT = (5, '18+')
+
+
+class FilmStatusEnum(Enum):
+    IN_RENT = (1, 'В прокате')
+    ANNOUNCEMENT = (2, 'Анонс')
