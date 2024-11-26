1ff64b79144e6c8b864a3490e4c9bd095924c301
mrlawsuit <opronin1992@gmail.com>
2024-11-26 11:08:39 +0200

    add base model

---
+++ core/database/base.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    def to_dict(self):
+        return {
+            column.name: self.enum_to_string(
+                getattr(self, column.name)
+            ) for column in self.__table__.columns
+        }
+
+    @staticmethod
+    def enum_to_string(value):
+        if isinstance(value, Enum):
+            return value.value
+        return value
