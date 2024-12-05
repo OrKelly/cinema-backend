2f978e31c5543964de09c9b367abbffcba91ce1a
mrlawsuit <opronin1992@gmail.com>
2024-12-05 20:13:16 +0200

    #KINO-9 добавил слой services для залов

---
+++ apps/cinema/services/halls.py
@@ -0,0 +1,76 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+from apps.cinema.repositories.halls import BaseHallsRepository
+from apps.cinema.models.halls import Hall
+from core.services.base import BaseOrmService
+from apps.cinema.exceptions.halls import HallNotFoundException
+
+
+@dataclass
+class BaseHallService:
+    repository: BaseHallsRepository
+
+    @abstractmethod
+    async def create(self, attributes: dict[str, Any]): ...
+
+    @abstractmethod
+    async def get_all(
+        self,
+        skip: int = 0,
+        limit: int = 100,
+        join_: set[str, Any] = None,
+        order_: dict | None = None,
+    ): ...
+
+    @abstractmethod
+    async def get_by_id(
+        self,
+        id_: int,
+        join_: set[str, Any] | None = None,
+    ) -> Hall | None: ...
+
+    @abstractmethod
+    async def get_by_filter(
+        self,
+        field: str,
+        value: Any,
+        join_: set[str, Any] = None,
+        order_: dict | None = None,
+    ): ...
+
+
+@dataclass
+class ORMHallService(BaseHallService, BaseOrmService):
+    async def create(self, attributes: dict[str, Any]):
+        return await super(BaseHallService, self).create(attributes)
+
+    async def get_all(
+            self,
+            skip: int = 0,
+            limit: int = 100,
+            join_: set[str] = None,
+            order_: dict | None = None,
+    ): ...
+
+    async def get_by_filter(
+            self,
+            field: str,
+            value: Any,
+            join_: set[str] = None,
+            order_: dict | None = None
+    ):
+        return await super(BaseHallService, self).get_by_filter(
+            field=field, value=value, join_=join_, order_=order_
+        )
+
+    async def get_by_id(
+            self, id_: int, join_: set[str] | None = None
+    ) -> Hall | None:
+        hall = await super(BaseHallService, self).get_by_id(
+            id_=id_, join_=join_
+        )
+        if not hall:
+            raise HallNotFoundException(hall_id=id_)
+        return hall
