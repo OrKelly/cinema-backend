250b6f7cfd64b56fa56c47b228cf504cc12db8b2
mrlawsuit <opronin1992@gmail.com>
2024-12-05 20:18:04 +0200

    #KINO-9 допилил ручку кинозалов

--- api/v1/cinema/handlers.py
+++ api/v1/cinema/handlers.py
@@ -5,22 +5,18 @@ from fastapi.requests import Request
 from api.v1.cinema.schemas import CreateHallSchema, CreateHallCompleteSchema
 from core.containers import get_container
 from core.schemas.responses.api_response import ApiResponse
-from apps.cinema.repositories.halls import ORMHallRepository, BaseHallsRepository
+from apps.cinema.use_cases.hall_create import CreateHallUseCase
 
 router = APIRouter()
 
 
-# @router.post('api/v1/cinema/halls')
-# async def create_hall_handler(
-#     request: Request,
-#     hall_data: CreateHallSchema,
-#     container=Depends(get_container),   # noqa: B008
-# ) -> ApiResponse[CreateHallCompleteSchema]:
-#     repository: ORMHallRepository = container.resolve(BaseHallsRepository)
-#     hall_data = hall_data.model_dump()
-#     hall = await repository.create()
-
-if __name__ == '__main__':
-    container=Depends(get_container)
-    repository: ORMHallRepository = container.resolve(BaseHallsRepository)
-    print(repository)
+@router.post('api/v1/cinema/halls')
+async def create_hall_handler(
+    request: Request,
+    hall_data: CreateHallSchema,
+    container=Depends(get_container),   # noqa: B008
+) -> ApiResponse[CreateHallCompleteSchema]:
+    use_case: CreateHallUseCase = container.resolve(CreateHallUseCase)
+    hall_data = hall_data.model_dump()
+    hall = await use_case.execute(hall_data=hall_data)
+    return ApiResponse(data=CreateHallCompleteSchema(id=hall.id))
