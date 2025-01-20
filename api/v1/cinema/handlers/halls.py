b67b1cc8ff2d2b59808bb2d52948bbe307612db2
osminin <osminin555@bk.ru>
2025-01-20 16:08:40 +0500

    #KINO-19 реализована ручка получения зала по id

--- api/v1/cinema/handlers/halls.py
+++ api/v1/cinema/handlers/halls.py
@@ -1,8 +1,12 @@
-from fastapi import Depends
+from typing import Annotated
+
+from fastapi import Depends, Path
 from fastapi.requests import Request
 from fastapi.routing import APIRouter
 
 from api.v1.cinema.schemas import CreateHallCompleteSchema, CreateHallSchema
+from api.v1.cinema.schemas.halls import GetHallSchema
+from apps.cinema.services.halls import BaseHallService
 from apps.cinema.use_cases.hall_create import CreateHallUseCase
 from core.containers import get_container
 from core.schemas.responses.api_response import ApiResponse
@@ -22,3 +26,14 @@ async def create_hall_handler(
     return ApiResponse(
         data=CreateHallCompleteSchema(id=hall.id, title=hall.title)
     )
+
+
+@router.get("/{id}")
+async def get_hall_handler(
+    request: Request,
+    id: Annotated[int, Path(gt=0, description="Enter hall id")],
+    container=Depends(get_container),  # noqa: B008
+) -> ApiResponse[GetHallSchema]:
+    hall_service: BaseHallService = container.resolve(BaseHallService)
+    hall = await hall_service.get_with_rows_and_places_by_id(id)
+    return ApiResponse(data=GetHallSchema.to_schema(hall))
