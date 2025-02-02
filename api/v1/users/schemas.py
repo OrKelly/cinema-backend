256bc4d7a4030bc37ba47a99753386a0f2aa65a5
mrlawsuit <opronin1992@gmail.com>
2025-02-02 20:26:24 +0200

    #KINO-15 добавил схемы для сотрудников

--- api/v1/users/schemas.py
+++ api/v1/users/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
+
+from apps.users.exceptions.auth import InvalidFieldException
 
 
 class UserRegisterSchema(BaseModel):
@@ -17,3 +19,26 @@ class UserRegisterCompleteSchema(BaseModel):
 class UserLoginSchema(BaseModel):
     email: EmailStr
     password: str
+
+
+class NewEmployeeRegisterSchema(BaseModel):
+    first_name: str
+    last_name: str
+    patronymic: str = None
+    email: EmailStr
+
+
+class EmployeeRegisterSchema(BaseModel):
+    user_id: int = None
+    employee_data: NewEmployeeRegisterSchema = None
+
+    @field_validator('employee_data', mode='before')
+    def employee_data_validator(cls, value, info):
+        if info.data.get('user_id') is not None:
+            raise InvalidFieldException
+        return value
+
+
+class EmployeeRegisterCompleteSchema(BaseModel):
+    id: int
+    status: str = Field(default="Сотрудник успешно зарегистрирован")
