11ca4a7b5096d3852f66f2c82c49fa0d12062831
osminin <osminin555@bk.ru>
2025-04-03 17:56:13 +0500

    #KINO-38 добавлен тест ручки

--- apps/cinema/use_cases/free_hall_places.py
+++ apps/cinema/use_cases/free_hall_places.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+from apps.cinema.models import Hall
 from apps.cinema.services.halls import BaseHallService
 from apps.films.services.film_sessions import BaseFilmSessionService
 from apps.orders.services.orders import BaseOrderService
@@ -11,14 +12,14 @@ class GetFreeHallPlaces:
     hals_service: BaseHallService
     order_service: BaseOrderService
 
-    async def execute(self, filmsession_id: int):
+    async def execute(self, filmsession_id: int) -> tuple[Hall, set]:
         filmsession = await self.filmsession_service.get_by_id(
             id_=filmsession_id, join_={"hall"}
         )
         orders = await self.order_service.get_by_filter(
             filter_params={"session_id": filmsession_id}
         )
-        taken_places = [order.place_id for order in orders]
+        taken_places = {order.place_id for order in orders}
         hall = await self.hals_service.get_by_id(
             id_=filmsession.hall_id, join_={"rows"}
         )
